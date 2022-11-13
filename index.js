5a41ece344a1a357bbfe05e8bec80b1e9f425227
Mohammed Diary Mohammed <hamadiary1@gmail.com>
2022-11-13 17:11:26 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1=2
+let num2=31
+let num3=14
+let num4=5
+const multiply = num1*num2
+const random = Math.floor(Math.random()) + 1;
+const mod = num3%num4;
+const max = Math.max(20);
